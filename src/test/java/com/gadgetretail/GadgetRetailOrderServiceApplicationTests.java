0507190a15b42b62bedfb3da1a40cfaed615b69a
Rajesh Takkellapati <58281464+rajeshtakkellapati@users.noreply.github.com>
2025-06-07 16:42:33 -0400

    Initial Commit - Bootstrapped project

---
+++ src/test/java/com/gadgetretail/GadgetRetailOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gadgetretail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadgetRetailOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
